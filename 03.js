3f559b6edc9e84908b1b9a29e09e3909ac681e75
Pavlo Efimov <pavlo_efimov@ukr.net>
2017-09-18 20:32:35 +0300

    js homework 4_amendment on task 3

--- 03.js
+++ 03.js
@@ -1,12 +1,15 @@
-document.getElementById('numbering').onclick=f1
+document.getElementById('numbering').onclick = f1
 
 function f1() {
-	
-	var elements = document.getElementsByTagName('p');
-	for (var i = 0; i < elements.length; i++) {
-    var input = elements[i];
-    var n = i+1;
-    input.innerHTML='№'+n+': '+input.innerHTML;
-	}
-	
+
+	var n = 0;
+    var elements = document.getElementById('task3').childNodes;
+    for (var i = 0; i < elements.length; i++) {
+        var input = elements[i];
+        
+        if (input.nodeName == "#text") continue;
+        n++;
+        input.innerHTML = '№' + n + ': ' + input.innerHTML;
+        
+    }
 }
